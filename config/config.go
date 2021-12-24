65c526909aff1f4c5119e85154f47205cd5232b5
Matt Bargenquast <matt.bargenquast@gmail.com>
2021-12-24 11:18:50 +1000

    Update

---
+++ config/config.go
@@ -0,0 +1,9 @@
+package config
+
+const (
+	// OperatorName is the name of the operator
+	OperatorName string = "ocm-agent-operator"
+	// OperatorNamespace is the namespace of the operator
+	OperatorNamespace string = "openshift-ocm-agent-operator"
+)
+
